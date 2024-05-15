1e22e3c63c9d6b39efbe25364dacbcf92fa7bb5e
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-05-15 11:30:41 +0200

    Add files via upload

---
+++ Jour01/Job13/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Créer le serveur HTTP
+const server = http.createServer((req, res) => {
+    let filePath = '.'; // Chemin de base pour les fichiers HTML
+
+    switch(req.url) {
+        case '/':
+            filePath += '/index.html';
+            break;
+        case '/about':
+            filePath += '/about.html';
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end("<h1>Page non trouvée</h1>");
+            return;
+    }
+
+    // Construire le chemin complet du fichier
+    filePath = path.join(__dirname, filePath);
+
+    // Lire le fichier HTML
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            res.end("<h1>Erreur interne du serveur</h1>");
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        }
+    });
+});
+
+// Démarrer le serveur sur le port 8888
+const port = 8888;
+server.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}.`);
+});
