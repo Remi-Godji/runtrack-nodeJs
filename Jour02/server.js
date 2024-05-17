ceb0cc6ea89d1d1325a206f4021e5cd7eb5988f9
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-05-17 15:32:09 +0200

    Delete Jour02 directory

--- Jour02/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-function handleRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}
-
-const server = http.createServer(handleRequest);
-module.exports = server; // Assurez-vous que le serveur est exporté
